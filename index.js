9584c633e5694dff318f279a4f5ff8698a909689
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-01-27 18:27:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 const prompt = require('prompt-sync')
 const { exit } = require('process')
 const util = require('./memecli.util')
+const inquirer = require('inquirer')
 
 console.log( '\x1b[32m\x1b[1m', 
 ` 
-+--------------------------------------------------------------------------+
-| ███   ███   ███████   ███   ███   ███████          ███████   ██       ██ |
-| ████ ████   ██        ████ ████   ██               ██        ██       ██ |
-| ██ ███ ██   ███████   ██ ███ ██   ███████   █████  ██        ██       ██ |
-| ██     ██   ██        ██     ██   ██               ██        ██       ██ |
-| ██     ██   ███████   ██     ██   ███████          ███████   ███████  ██ |
-+--------------------------------------------------------------------------+ 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+  ███   ███   ███████   ███   ███   ███████          ███████   ██       ██ 
+  ████ ████   ██        ████ ████   ██               ██        ██       ██ 
+  ██ ███ ██   ███████   ██ ███ ██   ███████   █████  ██        ██       ██ 
+  ██ ███ ██   ██        ██ ███ ██   ██               ██        ██       ██ 
+  ██     ██   ███████   ██     ██   ███████          ███████   ███████  ██ 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ 
 `, 
-'\x1b[0m' ) 
+'\x1b[0m' );
 
-var inp = prompt()('\u001b[36;1mEnter a meme (type %all for a popularity pie chart or %exit to exit) > \u001b[0m')
-if (inp === '%exit') {
-    exit(0)
-} else if (inp === '%all') {
-    (async()=>util.fetch_popularity())()
-} else {
-    (async()=>util.fetch_meme(inp))()
-}
-
+var opts = ['Fetch all memes and create a pie chart based on popularity', 'Fetch info for a particular meme', 'Exit']
+inquirer.prompt([
+    {
+        name: 'value',
+        message: 'What do you want to do?',
+        type: 'list',
+        choices: opts
+    }
+]).then(response => {
+    switch (response.value) {
+        case opts[0]: util.fetch_popularity(); break
+        case opts[1]: {
+            var inp = prompt()('\x1b[32m?\x1b[0m \x1b[1mEnter a meme > \x1b[0m')
+            util.fetch_meme(inp)
+            break
+        }
+        case opts[2]: exit(0)
+    }
+})
--- memecli.util.js
+++ memecli.util.js
@@ -6,7 +6,7 @@ const rand = (minimum, maximum) => { return Math.floor(Math.random() * (maximum 
 const mean = (arr) => { return arr.reduce((p,a) => p+a, 0)/arr.length }
 
 async function fetch_meme(inp) {
-    var res = await axios.get('http://memeengine.servehttp.com:5000/api/v1/memes/popular/stats')
+    var res = await axios.get('http://memeengine.servehttp.com:80/api/v1/memes/popular/stats')
     var d = res.data['response']
     var names = d.map(c => { return c['name'] })
     var object = d.reduce(
@@ -19,15 +19,19 @@ async function fetch_meme(inp) {
     fs.promises.writeFile(`${close}.html`, doc.data.replace('/*name*/', close).replace('/*data*/', y.toString()))
 }
 
-async function fetch_popularity() {
-    var res = await axios.get('http://memeengine.servehttp.com:5000/api/v1/memes/popular/stats')
-    var d = res.data['response']
-    var k = {}
-    d.forEach(v => { k[v['name']] = mean(v['trend']) })
-    
-    var doc = await axios.get('https://raw.githubusercontent.com/ajskateboarder/stuff/main/meme.js/pie.html')
-    var colors = Array.from({ length: Object.keys(k).length }, (_, k) => `'rgb(${rand(200,255)},${rand(200,255)},${rand(200,255)})'`);
-    fs.promises.writeFile('popularity.html', doc.data.replace('{/*data*/}', JSON.stringify(k)).replace('/*colors*/', colors.toString()))
+function fetch_popularity() {
+    axios.get('http://memeengine.servehttp.com:80/api/v1/memes/popular/stats')
+    .then(res => {
+        var d = res.data['response']
+        var k = {}
+        d.forEach(v => { k[v['name']] = mean(v['trend']) })
+        
+        axios.get('https://raw.githubusercontent.com/ajskateboarder/stuff/main/meme.js/pie.html')
+        .then(doc => {
+            var colors = Array.from({ length: Object.keys(k).length }, (_, k) => `'rgb(${rand(200,255)},${rand(200,255)},${rand(200,255)})'`);
+            fs.writeFileSync('popularity.html', doc.data.replace('{/*data*/}', JSON.stringify(k)).replace('/*colors*/', colors.toString()))
+        })
+    })
 }
 
 exports.fetch_meme = fetch_meme
