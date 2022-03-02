e4cdd68b5eaa3cc964a3a2d2e51f2d336d47421a
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-03-02 08:15:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,19 @@ setup(
     package_data={'library': ['library.py']},
     packages=find_packages('src'),
     entry_points={'console_scripts': ['memewizard = memewizard.cli:main'], },
+    install_requires=[
+     'thefuzz',
+     'prompt_toolkit==1.0.14',
+     'tqdm',
+     'requests',
+     'beautifulsoup4',
+     'html2image',
+     'tabulate',
+     'PyInquirer',
+     'sklearn',
+     'pandas',
+     'numpy',
+     'pytrends',
+     'matplotlib',   
+    ]
 )
-
