dcb72ad117047ebffef239eacdcc622a67b9fac9
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-03-08 14:19:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="memewizard",
-    version="0.1",
-    package_data={'library': ['library.py']},
-    packages=find_packages('src'),
+    version="0.0.5.1",
     entry_points={'console_scripts': ['memewizard = memewizard.cli:main'], },
     install_requires=[
      'thefuzz',
