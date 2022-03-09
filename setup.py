4205194b7fb08ead4fe65c0f883a6d05ec6d28a5
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-03-08 19:32:01 -0500

    im not ready for this to become a pypi package...

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from setuptools import setup, find_packages
+
+long_description = open('README.md', encoding='utf-8').read()
+
 setup(
     name="memewizard",
+    author='themysticsavages',
+    description='One good source for all things memes'
+    license='MIT',
     version="0.0.5.1",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=long_description,      
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     install_requires=[
      'thefuzz',
